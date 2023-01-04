2a1f5e295127b87f83ba7a50b1dce150e43fbccd
sahil9510 <srivastavasahil133@gmail.com>
2023-01-04 16:15:12 +0530

    update metadata url

--- nft-contract/constants/index.js
+++ nft-contract/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x912a9CFEc7492d22C1d5D3c1558a42bdeA519Cb2";
-const METADATA_URL = "";
+const METADATA_URL = "https://nft-collection-eight-rho.vercel.app/api/";
 
 module.exports={
     WHITELIST_CONTRACT_ADDRESS,METADATA_URL
